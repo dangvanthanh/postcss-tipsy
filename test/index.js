12d1500791fb48c0c5b96e1bab83635d3467de12
Dang Van Thanh <dangvanthanh@dangthanh.org>
2017-06-01 19:37:32 +0700

    Add tape for test

---
+++ test/index.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var test = require('tape')
+var postcss = require('postcss')
+var tipsy = require('..')
+
+function fixture (name) {
+  return fs.readFileSync('test/fixtures/' + name + '.css', 'utf-8').trim()
+}
+
+function output (name) {
+  return fs.readFileSync('test/fixtures/' + name + '.out.css', 'utf-8').trim()
+}
+
+function compare (name) {
+  return test(name, function (t) {
+    var res = postcss()
+              .use(tipsy)
+              .process(fixture(name))
+              .css.trim()
+    t.same(res, output(name))
+    t.end()
+  })
+}
+
+compare('test-1')
